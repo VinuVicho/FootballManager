5a8f9081f831614d71737c030c0a759c81920e93
VinuVicho <vovkamlad@gmail.com>
2022-02-16 22:36:12 +0200

    Yes

--- src/main/java/me/vinuvicho/footballmanager/exeption/PlayerNotFoundException.java
+++ src/main/java/me/vinuvicho/footballmanager/exeption/PlayerNotFoundException.java
@@ -2,7 +2,7 @@ package me.vinuvicho.footballmanager.exeption;
 
 import org.springframework.data.crossstore.ChangeSetPersister;
 
-public class PlayerNotFoundException extends /*RuntimeException*/ ChangeSetPersister.NotFoundException {
+public class PlayerNotFoundException extends RuntimeException  {
     public PlayerNotFoundException(String e) {
         System.out.println(e);
     }
